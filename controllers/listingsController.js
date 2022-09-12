d79adee1663ef292b193632d1f27567465b2740e
ryojif2 <ryojifuj2@gmail.com>
2022-09-12 19:31:48 +0800

    Exercise completed backend

--- controllers/listingsController.js
+++ controllers/listingsController.js
@@ -1,3 +1,4 @@
+const cors = require("cors");
 const BaseController = require("./baseController");
 
 class ListingsController extends BaseController {
@@ -9,11 +10,31 @@ class ListingsController extends BaseController {
   /** if a method in this extended class AND the base class has the same name, the one in the extended class will run over the base method */
   // Create listing. Requires authentication.
   async insertOne(req, res) {
-    const { title, category, condition, price, description, shippingDetails } =
-      req.body;
+    const {
+      title,
+      category,
+      condition,
+      price,
+      description,
+      shippingDetails,
+      sellerEmail,
+    } = req.body;
     try {
       // TODO: Get seller email from auth, query Users table for seller ID
 
+      let seller;
+
+      const [user, created] = await this.userModel.findOrCreate({
+        where: { email: sellerEmail },
+      });
+
+      if (created) {
+        seller = user.id;
+      } else {
+        seller = user.id;
+      }
+
+      console.log(seller);
       // Create new listing
       const newListing = await this.model.create({
         title: title,
@@ -22,8 +43,8 @@ class ListingsController extends BaseController {
         price: price,
         description: description,
         shippingDetails: shippingDetails,
-        BuyerId: null,
-        SellerId: 1, // TODO: Replace with seller ID of authenticated seller
+        buyerId: null,
+        sellerId: seller, // TODO: Replace with seller ID of authenticated seller
       });
 
       // Respond with new listing
@@ -47,11 +68,24 @@ class ListingsController extends BaseController {
   // Buy specific listing. Requires authentication.
   async buyItem(req, res) {
     const { listingId } = req.params;
+    const { buyerEmail } = req.body;
     try {
       const data = await this.model.findByPk(listingId);
 
+      let buyer;
+
+      const [user, created] = await this.userModel.findOrCreate({
+        where: { email: buyerEmail },
+      });
+
+      if (created) {
+        buyer = user.id;
+      } else {
+        buyer = user.id;
+      }
+
       // TODO: Get buyer email from auth, query Users table for buyer ID
-      await data.update({ BuyerId: 1 }); // TODO: Replace with buyer ID of authenticated buyer
+      await data.update({ buyerId: buyer }); // TODO: Replace with buyer ID of authenticated buyer
 
       // Respond to acknowledge update
       return res.json(data);
--- index.js
+++ index.js
@@ -1,25 +1,37 @@
-const cors = require('cors')
-const express = require('express')
-require('dotenv').config();
+const cors = require("cors");
+const express = require("express");
+const { auth } = require("express-oauth2-jwt-bearer");
+require("dotenv").config();
 
 // importing Routers
-const ListingsRouter = require('./routers/listingsRouter')
+const ListingsRouter = require("./routers/listingsRouter");
 
 // importing Controllers
-const ListingsController = require('./controllers/listingsController')
+const ListingsController = require("./controllers/listingsController");
 
 // importing DB
-const db = require('./db/models/index')
+const db = require("./db/models/index");
 const { listing, user } = db;
 
+const checkJwt = auth({
+  audience: process.env.AUDIENCE,
+  issuerBaseURL: process.env.ISSUER,
+});
+
 // initializing Controllers -> note the lowercase for the first word
-const listingsController = new ListingsController(listing, user)
+const listingsController = new ListingsController(listing, user);
 
 // inittializing Routers
-const listingsRouter = new ListingsRouter(listingsController).routes()
+const listingsRouter = new ListingsRouter(
+  listingsController,
+  checkJwt
+).routes();
 
 const PORT = process.env.PORT;
 const app = express();
+
+// Enable Auth0 middleware
+// app.use(auth());
 
 // Enable CORS access to this server
 app.use(cors());
@@ -28,7 +40,7 @@ app.use(cors());
 app.use(express.json());
 
 // enable and use router
-app.use('/listings', listingsRouter)
+app.use("/listings", listingsRouter);
 
 app.listen(PORT, () => {
   console.log(`Express app listening on port ${PORT}!`);
